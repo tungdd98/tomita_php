95a57b2cd9a3e407d11745a4bfc73cd7aad2cb77
Dang Duc Tung <tungdd98@gmail.com>
2020-07-11 23:39:45 +0700

    upload db

--- db/banhang.sql
+++ db/banhang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2020 at 09:05 PM
+-- Generation Time: Jul 11, 2020 at 06:39 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
